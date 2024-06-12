f2d6bf76ea8887ebd9bd7a8d48632eefefb098fc
Aryamann Verma <avverma19699@gmail.com>
2024-06-12 17:42:17 -0300

    fixed the styling issue for hamburger
    had the scope wrapped incorrectly for the medie query in global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: "https://stunning-fenglisu-7a82af.netlify.app",
-  integrations: [preact()]
+  integrations: [preact()],
+  devToolbar: {
+    enabled: false
+  }
 });
